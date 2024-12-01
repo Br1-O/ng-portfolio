0f4864f7da6bdddc91df39dc299035af918f141f
Br1-O <bruno.ortuno2@gmail.com>
2024-12-01 08:04:48 -0300

    Feat: Added icons from boxicons and loading overlay

--- src/app/app-component/app.component.ts
+++ src/app/app-component/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { FooterComponent } from '../components/footer/footer.component';
@@ -11,6 +11,19 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements AfterViewInit {
+  isLoading = true;  // Start with loading overlay visible
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    document.title = 'Loading...';
+
+    this.renderer.listen('document', 'readystatechange', () => {
+      if (document.readyState === 'complete') {
+        this.isLoading = false;
+        document.title = 'Bruno Ortuño | Portfolio';
+      }
+    });
+  }
 }
--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BadgeComponent } from '../badge/badge.component';
 import { CommonModule } from '@angular/common';
 
@@ -10,6 +10,10 @@ import { CommonModule } from '@angular/common';
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.css'
 })
-export class ProjectCardComponent {
+export class ProjectCardComponent implements OnInit {
+  
   @Input() project: any;
+
+  ngOnInit(): void {
+  }
 }
