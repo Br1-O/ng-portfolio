6a85da07f3b67ca03923d97bce1a234622389e1b
Br1-O <bruno.ortuno2@gmail.com>
2024-12-04 22:56:09 -0300

    Fix: Improved fetched data formating for projects section

--- src/app/pages/home/sections/projects/projects.component.ts
+++ src/app/pages/home/sections/projects/projects.component.ts
@@ -5,10 +5,10 @@ import { ButtonArrowComponent } from '../../../../components/button-arrow/button
 
 export interface Repository {
   name: string;
-  html_url: string;
-  homepage: string;
+  html_url: string | null;
+  homepage: string | null;
   description: string;
-  img: string;
+  img: string | null;
   topics: string[];
 }
 
@@ -25,6 +25,22 @@ export class ProjectsComponent implements OnInit {
   projects: Repository[] = [];
 
   ngOnInit(): void {
+
+    //add private projects cards
+
+    let privateProject: Repository = 
+    {
+      name: "NextFlix",
+      html_url: null,
+      homepage: "https://nextflix-br1.vercel.app/",
+      description: "Un clon fullstack funcional de Netflix, usando su propia base de datos para peliculas y usuarios.",
+      img: "https://i.imgur.com/0cG22c7.png",
+      topics: ["React.js", "Next.js", "JWT", "MongoDB"],
+    };
+
+    this.projects.push(privateProject);
+
+    //fetch public repositories
     fetch(`https://api.github.com/users/${this.user}/repos?per_page=100&page=1`)    
     .then(response => response.json())
     .then(data => 
@@ -35,10 +51,9 @@ export class ProjectsComponent implements OnInit {
     .catch(error => console.error('Error:', error));
   }
 
+  //filter to only show personal projects
   getOnlyPersonalProjects(data: Repository[]): void {
     let indexKeyWord = 0;
-
-    console.log(data);
     
     data.forEach((project) => {
   
@@ -49,18 +64,48 @@ export class ProjectsComponent implements OnInit {
       if (indexKeyWord !== -1) {
         // Push the project into the array
         this.projects.push(project);
-  
-        // Set the 'img' property using the last added project
-        this.projects[this.projects.length - 1].img = project.description.substring((indexKeyWord + 11), project.description.indexOf("]"));
+
+        // Normalize project's name
+        let formattedName =  ((this.projects[this.projects.length - 1].name).replaceAll("-", " ")).replaceAll("_", " ");
+
+        formattedName = formattedName.split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+
+        this.projects[this.projects.length - 1].name = formattedName;
+
+        // Set proper project's image
+        if ((project.description).includes("[PROJECT -")) {
+          // Set the 'img' property using the last added project
+          this.projects[this.projects.length - 1].img = project.description.substring((indexKeyWord + 11), project.description.indexOf("]"));
+        }else{
+          this.projects[this.projects.length - 1].img = "images/logo.jpg";
+        }
 
         // Set proper description
-        this.projects[this.projects.length - 1].description = project.description.substring(0, indexKeyWord);
+        if ((project.description).includes("[PROJECT") && (project.description).includes("{")) {
+          let desiredLang= "es";
+
+          switch (desiredLang) {
+            case "en":
+              let indexStartDescriptionEnglish = project.description.indexOf("{ENGLISH:");
+              let indexEndDescriptionEnglish = project.description.indexOf("}");
+
+              this.projects[this.projects.length - 1].description = project.description.substring(indexStartDescriptionEnglish + 11, indexEndDescriptionEnglish);
+            break;
+          
+            case "es":
+              let indexStartDescriptionSpanish = project.description.indexOf("(ESPAÑOL:");
+              let indexEndDescriptionSpanish = project.description.indexOf(")");
+
+              this.projects[this.projects.length - 1].description = project.description.substring(indexStartDescriptionSpanish + 10, indexEndDescriptionSpanish);
+            break;
+          }
+        }
 
         // Set the labels for languages and technologies
         this.projects[this.projects.length - 1].topics = project.topics;
       }
     });
-
-    console.log(this.projects);
   }
 }
