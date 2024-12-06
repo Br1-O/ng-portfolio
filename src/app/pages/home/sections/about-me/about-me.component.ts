40eba24fe1d5dabb66a0deb4f728cb906ef0c4bd
Br1-O <bruno.ortuno2@gmail.com>
2024-12-06 19:01:15 -0300

    Fix: Changes in about-me style

--- src/app/pages/home/sections/about-me/about-me.component.ts
+++ src/app/pages/home/sections/about-me/about-me.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { ButtonArrowComponent } from '../../../../components/button-arrow/button-arrow.component';
-import { CurrvitaeBtnsComponent } from '../../../../components/currvitae-btns/currvitae-btns.component';
 
 @Component({
   selector: 'app-about-me',
-  imports: [ButtonArrowComponent, CurrvitaeBtnsComponent],
+  imports: [ButtonArrowComponent],
   standalone: true,
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
